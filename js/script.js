58070b658e8d318bc8f311a1f29f2bc7b6764a0f
Edyta Szarowska <edyta.szarowska@interia.pl>
2019-08-06 13:34:19 +0200

    fix bug in titleClickHandler

--- js/script.js
+++ js/script.js
@@ -1,44 +1,28 @@
 'use strict'
 
-// document.getElementById('test-button').addEventListener('click', function () {
-//     const links = document.querySelectorAll('.titles a');
-//     console.log('links:', links);
-// });
-
 const titleClickHandler = function (event) {
-    const clickedElement = this;
-    console.log('Link was clicked!');
 
+    const clickedElement = this;
     event.preventDefault();
 
     /*[DONE] remove class 'active' from all article links  */
 
-    const activeLinks = document.querySelectorAll('.titles a.active');
-
-    for (let activeLink of activeLinks) {
-        activeLink.classList.remove('active');
-    }
+    const activeLinks = document.querySelector('.titles a.active').classList.remove('active');
 
     /* [IN PROGRESS] add class 'active' to the clicked link */
-    console.log('clickedElement:', clickedElement);
-    this.classList.add('active');
+    clickedElement.classList.add('active');
 
     /*[DONE] remove class 'active' from all articles */
-    const activeArticles = document.querySelectorAll('.posts article.active');
-    for (let activeArticle of activeArticles) {
-        activeArticle.classList.remove('active');
-    }
+    const activeArticles = document.querySelector('.posts article.active').classList.remove('active');
 
     /* get 'href' attribute from the clicked link */
-
     const articleSelector = clickedElement.getAttribute('href');
-    console.log(articleSelector);
 
     /* find the correct article using the selector (value of 'href' attribute) */
-    const targetArticle = querySelector('article -');
-    console.log(targetArticle);
+    const targetArticle = document.querySelector(articleSelector);
 
     /* add class 'active' to the correct article */
+    targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
