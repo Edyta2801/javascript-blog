a8df7f5ebc155b95aa4f7a0b438192dff7879649
Edyta Szarowska <edyta.szarowska@interia.pl>
2019-09-27 08:44:27 +0200

    restore changes with partly authors filtering

--- js/script.js
+++ js/script.js
@@ -432,6 +432,7 @@ addClickListenersToAuthors();
 
 
 function authorClickHandler(event) {
+
   /* prevent default action for this event */
   event.preventDefault();
   /* make new constant named "clickedElement" and give it the value of "this" */
@@ -446,7 +447,7 @@ function authorClickHandler(event) {
   for (let activeAuthorLink of activeAuthorLinks) {
     console.log(activeAuthorLink);
     /* remove class active */
-    activeAuthorLink.classList.remove('active');
+    activeAuthorLinks.classList.remove('active');
     /* END LOOP: for each active author link */
   }
   /* find all author links with "href" attribute equal to the "href" constant */
@@ -464,12 +465,12 @@ function authorClickHandler(event) {
 
 function addClickListenersToAuthors() {
   /* find all links to tags */
-  // const authorLinks = document.querySelectorAll('.post p .post-autor');
-  const authorLinks = document.querySelectorAll('.post-author .list a');
-  console.log(authorLinks);
+  // const authorsLinks = document.querySelectorAll('.post p .post-autor');
+  const authorsLinks = document.querySelectorAll('.post-author  a');
+  console.log(authorsLinks);
 
   /* START LOOP: for each link */
-  for (let authorLink of authorLinks) {
+  for (let authorLink of authorsLinks) {
 
     /* add tagClickHandler as event listener for that link */
     authorLink.addEventListener('click', authorClickHandler);
@@ -500,6 +501,5 @@ function addClickListenersToAuthors() {
 // }
 
 // addClickListenersSidebarToAuthors();
-
 
 
