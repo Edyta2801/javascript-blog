73331cc6a00d393fd1c84460bc497cc2abe6b783
Edyta Szarowska <edyta.szarowska@interia.pl>
2019-10-01 09:50:23 +0200

    add Handlebars for link to article

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 'use strict'
 
+const templates = {
+  articleLink: Handlebars.compile(document.querySelector('#template-article-link').innerHTML)
+}
+
 // const opt={
 //   articleSelector : '.post',
 //   titleSelector : '.post-title',
@@ -113,7 +117,9 @@ function generateTitleLinks(customSelector = '') {
     const articleTitle = article.querySelector(select.all.titleSelector).innerHTML;
 
     /* create HTML of the link */
-    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    // const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    const linkHTMLData = {id: articleId, title: articleTitle};
+    const linkHTML = templates.articleLink(linkHTMLData);
     console.log(linkHTML);
 
 
